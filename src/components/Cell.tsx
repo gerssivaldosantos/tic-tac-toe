5887a9bed9b8e2d3d452b52ced9c626150b5059c
gerssivaldo <canalandows@gmail.com>
2023-08-12 20:36:07 -0300

    feat: add Cell component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,4 +1,19 @@
-export default function Cell() {
-    return <>
-    </>
+
+/* O que esse componente deve fazer?
+* - Receber o símbolo a ser exibido dentro do botão;
+* - Receber um Id que dá identidade única para esta célula
+* - Emitir um evento de clique leva o Id como callback para indicar qual célula que foi marcada
+*  */
+
+type props = {
+    id: string,
+    symbol: string,
+    onPlay: (id: string) => void
+}
+
+export default function Cell({id, symbol, onPlay}: props) {
+    console.log(symbol)
+    return <button onClick={() => onPlay(id)}>
+        {symbol}
+    </button>
 }
