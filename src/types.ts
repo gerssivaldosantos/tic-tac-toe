7476fde46580cecfb2828253f1629a1bf3f7f594
gerssivaldo <canalandows@gmail.com>
2023-08-12 21:37:55 -0300

    feat: add types

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export type TableState = {
+    1: gameSymbol| '';
+    2: gameSymbol| '';
+    3: gameSymbol| '';
+    4: gameSymbol| '';
+    5: gameSymbol| '';
+    6: gameSymbol| '';
+    7: gameSymbol| '';
+    8: gameSymbol| '';
+    9: gameSymbol| '';
+};
+
+export type CellIds = Array<CellId>;
+
+export type CellId = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
+export type gameSymbol = 'X' | 'O'
+
+export type play = {
+    id: CellId,
+    symbol: gameSymbol
+}
+
+export type GameResume = Record<number, play>
