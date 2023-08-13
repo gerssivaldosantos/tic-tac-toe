4821f3e6632376d0908fe89081ee3186b6a09f97
gerssivaldo <canalandows@gmail.com>
2023-08-12 21:38:21 -0300

    feat: get props

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,4 +1,11 @@
-export default function GameBoard() {
+import {GameResume} from "../types.ts";
+
+type props = {
+    gameResume: GameResume
+}
+
+export default function GameBoard({gameResume}: props) {
+    console.log(gameResume)
     return <>
     </>
 }
