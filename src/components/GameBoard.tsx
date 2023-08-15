bd302499f6aa5b5d1c644f87eb84c165b57fe59b
gerssivaldo <canalandows@gmail.com>
2023-08-15 01:06:05 -0300

    feat: add key for jsx at gameBoard

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -6,9 +6,8 @@ type props = {
 }
 
 export default function GameBoard({gameResume}: props) {
-    console.log(gameResume)
     const resumes: JSX.Element[] = gameResume.map((value: play, index: number) => {
-       return <li><h3>{index + 1}º - {value.symbol} em #{value.id}</h3></li>
+       return <li key={index}><h3>{index + 1}º - {value.symbol} em #{value.id}</h3></li>
     })
     return <>
         <ul>
