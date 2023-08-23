60f96ec996073ca8241e95a24405bf02b26e5da5
gerssivaldo <canalandows@gmail.com>
2023-08-23 00:15:34 -0300

    feat: add test for Table (basic structure)

---
+++ src/tests/Table.spec.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import {describe, it, expect} from "vitest";
+
+
+describe('Table Component', () => {
+
+    it('Should instance a Table successfully', () => {
+        expect(true).toBeTruthy()
+    })
+
+})
